7753db1f1a6e23f32f953c6cdba7593476a8eb45
ssgssdlbeck-hash <ssgssdlbeck@gmail.com>
2025-12-11 19:16:59 -0800

    Add files via upload

---
+++ legacy-universe-tracker/service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "legacy-universe-cache-v1";
+const URLS_TO_CACHE = [
+  "legacy-universe-tracker.html",
+  "manifest.webmanifest"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
+
